7aff6550e944d5fa813436e95e972eccd3e7c1f6
sunnyJha08 <sunnyjha98971@gmail.com>
2025-07-08 23:37:49 +0530

    Add files via upload
    remaining source files of index.html

---
+++ Prerequisites/index.js
@@ -0,0 +1,15 @@
+// This is a single line Comment
+
+/* This 
+   is 
+   a 
+   multi-line
+   comment */
+
+console.log(`Hello`);
+console.log(`I like programming`);
+
+window.alert(`JavaScript is such a unique language`);
+
+document.getElementById("myFirstH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like Programming languages`;
