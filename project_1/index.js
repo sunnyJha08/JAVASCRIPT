0098e76aa4f58b1342ceb92603dfcc22a83f9ac1
sunnyJha08 <sunnyjha98971@gmail.com>
2025-07-19 23:15:47 +0530

    Add files via upload

---
+++ project_1/index.js
@@ -0,0 +1,46 @@
+const users = [
+  {
+    name: "abc xyz",
+    pic: "https://images.unsplash.com/photo-1647129862028-6f67a76bd04b?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDcxfHx8ZW58MHx8fHx8",
+    bio: "silent chaos in a loud world | not for everyone",
+  },
+  {
+    name: "Liam Carter",
+    pic: "https://images.unsplash.com/photo-1752144497057-ef0bc73e7fd2?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D",
+    bio: "walking paradox with a caffeine addiction",
+  },
+  {
+    name: "Maya Singh",
+    pic: "https://images.unsplash.com/photo-1752496906365-d5c662900cc1?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D",
+    bio: "dreaming in pixels and thinking in code",
+  },
+  {
+    name: "Zane Brooks",
+    pic: "https://images.unsplash.com/photo-1752312641088-3c7425261a5c?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE4fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D",
+    bio: "sarcasm is my love language",
+  },
+  {
+    name: "Aarav Mehta",
+    pic: "https://images.unsplash.com/photo-1751719036803-5d85c5351368?q=80&w=866&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    bio: "just vibing through the noise",
+  },
+  {
+    name: "Sofia Morales",
+    pic: "https://images.unsplash.com/photo-1562312899-5350af13a222?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8",
+    bio: "lover of sunsets, silence, and strange ideas",
+  },
+  {
+    name: "Noah Lee",
+    pic: "https://images.unsplash.com/photo-1582558005893-1e017f3ce10a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHx8",
+    bio: "the quiet one with loud thoughts",
+  },
+  {
+    name: "Isla Raymond",
+    pic: "https://images.unsplash.com/photo-1559077432-ba2bcadcf3cb?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDUyfHx8ZW58MHx8fHx8",
+    bio: "in love with the stars and lost places",
+  },
+];
+
+function showUsers(arr) {
+       
+}
